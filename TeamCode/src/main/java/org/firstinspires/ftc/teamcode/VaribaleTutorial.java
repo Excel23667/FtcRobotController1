056f145181831d1d2f3fd94b5b2511329e15cd26
Excel23667 <ExcelRobotics23667@gmail.com>
2025-09-09 20:57:31 -0700

    Create VaribaleTutorial.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VaribaleTutorial.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class VaribaleTutorial extends OpMode {
+    /* data types
+    int robotHeight;
+    double motorSpeed;
+    decimal
+    boolean clawClosed;
+    yes or no
+
+    robotHeight = 10;
+    motorSpeed = 0.5;
+    clawClosed = true;
+     */
+
+    @Override
+    public void init() {
+        int teamNumber = 23667;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        String name = "Nano Ninjas";
+        int motorAngle = 67;
+
+        telemetry.addData("Team Number",teamNumber);
+        telemetry.addData("Motor Speed",motorSpeed);
+        telemetry.addData("Claw Closed",clawClosed);
+        telemetry.addData("Name",name);
+        telemetry.addData("Motor Angle",motorAngle);
+    }
+
+    @Override
+    public void loop() {
+        /*
+        1. change the string variable name to team name
+        2. create an integer variable called motor angle between 0-100, display this in your init method.
+         */
+
+    }
+}
