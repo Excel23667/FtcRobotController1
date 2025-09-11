1341d26b326ab65beac5e8e50ea950046f7760de
Excel23667 <ExcelRobotics23667@gmail.com>
2025-09-10 20:56:13 -0700

    midway through tutorial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLocationPractice.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public class RobotLocationPractice {
+    double angle;
+
+    //constructor method
+    public RobotLocationPractice(double angle){
+        this.angle = angle; 
+    }
+}
