0d5aaef2f4d83f2af0f049692b8fdf8461da13da
Excel23667 <ExcelRobotics23667@gmail.com>
2025-09-16 14:20:51 -0700

    wowza

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumReal.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+@Disabled
+public class TestMecanumReal extends OpMode {
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
