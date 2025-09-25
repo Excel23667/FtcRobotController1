3a2841a75ec2eb44d1896e7884cd48ad5afbcde0
Excel23667 <ExcelRobotics23667@gmail.com>
2025-09-25 14:46:13 -0700

    dsv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Quinolus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Quinolus.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
+@Disabled
 public class Quinolus extends OpMode {
 
     private DcMotor motor1;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumReal.java
@@ -3,18 +3,67 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
 @TeleOp
-@Disabled
+
 public class TestMecanumReal extends OpMode {
 
+    private DcMotor FrontLeft;
+    private DcMotor BackRight;
+    private DcMotor BackLeft;
+    private DcMotor FrontRight;
+    private DcMotor Shooter;
+    private DcMotor Intake;
+    float y;
+    double x;
+    double rx;
+    double denominator;
+
     @Override
-    public void loop() {
+    public void init() {
+
+        FrontLeft = hardwareMap.get(DcMotor.class, "Front Left");
+        BackRight = hardwareMap.get(DcMotor.class, "Back Right");
+        BackLeft = hardwareMap.get(DcMotor.class, "Back Left");
+        FrontRight = hardwareMap.get(DcMotor.class, "Front Right");
+        Shooter = hardwareMap.get(DcMotor.class, "Shooter");
+        Intake = hardwareMap.get(DcMotor.class, "Intake");
+
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.FORWARD);
+        Shooter.setDirection(DcMotor.Direction.REVERSE);
+        Intake.setDirection(DcMotor.Direction.REVERSE);
 
     }
 
     @Override
-    public void init() {
+    public void loop() {
+
+        telemetry.addData("Left Stick X",gamepad1.left_stick_x);
+        telemetry.addData("Right Stick X",gamepad1.right_stick_y);
+        telemetry.addData("Right Stick Y",gamepad1.right_stick_y);
+        telemetry.update();
+        y = -gamepad1.right_stick_y * 1;
+        x = gamepad1.right_stick_x * 1.1 * -1;
+        rx = -gamepad1.left_stick_x * 0.6 * 1;
+        denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(y), Math.abs(x), Math.abs(rx))), 1));
+        BackLeft.setPower((y + x + rx) / denominator);
+        FrontLeft.setPower(((y - x) + rx) / denominator);
+        BackRight.setPower(((y - x) - rx) / denominator);
+        FrontRight.setPower(((y + x) - rx) / denominator);
+        if (gamepad1.a){
+            Shooter.setPower(0.6);
+        } else if (gamepad1.b) {
+            Shooter.setPower(0);
+        }
+
+        Intake.setPower(gamepad1.right_trigger*0.75);
 
     }
 }
