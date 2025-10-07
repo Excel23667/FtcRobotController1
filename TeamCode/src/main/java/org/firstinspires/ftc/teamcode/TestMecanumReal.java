e541ff8c4626f1fd72c80b080d1808eecf3673b5
Excel23667 <ExcelRobotics23667@gmail.com>
2025-10-07 14:49:03 -0700

    Update TestMecanumReal.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumReal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanumReal.java
@@ -26,39 +26,42 @@ public class TestMecanumReal extends OpMode {
     @Override
     public void init() {
 
-        FrontLeft = hardwareMap.get(DcMotor.class, "Front Left");
-        BackRight = hardwareMap.get(DcMotor.class, "Back Right");
-        BackLeft = hardwareMap.get(DcMotor.class, "Back Left");
-        FrontRight = hardwareMap.get(DcMotor.class, "Front Right");
+        FrontLeft = hardwareMap.get(DcMotor.class, "Front Right");
+        BackRight = hardwareMap.get(DcMotor.class, "Front Left");
+        BackLeft = hardwareMap.get(DcMotor.class, "Back Right");
+        FrontRight = hardwareMap.get(DcMotor.class, "Back Left");
         Shooter = hardwareMap.get(DcMotor.class, "Shooter");
         Intake = hardwareMap.get(DcMotor.class, "Intake");
 
         FrontLeft.setDirection(DcMotor.Direction.REVERSE);
         BackRight.setDirection(DcMotor.Direction.REVERSE);
         BackLeft.setDirection(DcMotor.Direction.REVERSE);
-        BackLeft.setDirection(DcMotor.Direction.FORWARD);
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);
         Shooter.setDirection(DcMotor.Direction.REVERSE);
         Intake.setDirection(DcMotor.Direction.REVERSE);
+
+        //double minustrig =
 
     }
 
     @Override
     public void loop() {
+        //double minustrig = gamepad1.left_trigger *0.6;
 
         telemetry.addData("Left Stick X",gamepad1.left_stick_x);
         telemetry.addData("Right Stick X",gamepad1.right_stick_y);
         telemetry.addData("Right Stick Y",gamepad1.right_stick_y);
         telemetry.update();
         y = -gamepad1.right_stick_y * 1;
-        x = gamepad1.right_stick_x * 1.1 * -1;
-        rx = -gamepad1.left_stick_x * 0.6 * 1;
+        x = -gamepad1.right_stick_x * 1.1 * -1;
+        rx = gamepad1.left_stick_x * 0.6 * 1;
         denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(y), Math.abs(x), Math.abs(rx))), 1));
         BackLeft.setPower((y + x + rx) / denominator);
         FrontLeft.setPower(((y - x) + rx) / denominator);
         BackRight.setPower(((y - x) - rx) / denominator);
         FrontRight.setPower(((y + x) - rx) / denominator);
         if (gamepad1.a){
-            Shooter.setPower(0.6);
+            Shooter.setPower(0.6 - (gamepad1.left_trigger*0.6));
         } else if (gamepad1.b) {
             Shooter.setPower(0);
         }
