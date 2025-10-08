376baf35bb3b8ab29a5bacdbf2bf2c7c01b95a6b
Excel23667 <ExcelRobotics23667@gmail.com>
2025-10-08 15:10:22 -0700

    jhv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewChassis.java
@@ -9,8 +9,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
 @TeleOp
-@Disabled
-public class TestMecanumReal extends OpMode {
+
+public class NewChassis extends OpMode {
 
     private DcMotor FrontLeft;
     private DcMotor BackRight;
@@ -18,7 +18,7 @@ public class TestMecanumReal extends OpMode {
     private DcMotor FrontRight;
     private DcMotor Shooter;
     private DcMotor Intake;
-    float y;
+    double y;
     double x;
     double rx;
     double denominator;
@@ -26,47 +26,30 @@ public class TestMecanumReal extends OpMode {
     @Override
     public void init() {
 
-        FrontLeft = hardwareMap.get(DcMotor.class, "Front Right");
-        BackRight = hardwareMap.get(DcMotor.class, "Front Left");
-        BackLeft = hardwareMap.get(DcMotor.class, "Back Right");
-        FrontRight = hardwareMap.get(DcMotor.class, "Back Left");
-        Shooter = hardwareMap.get(DcMotor.class, "Shooter");
-        Intake = hardwareMap.get(DcMotor.class, "Intake");
+        FrontLeft = hardwareMap.get(DcMotor.class, "Front Left");
+        BackRight = hardwareMap.get(DcMotor.class, "Back Right");
+        BackLeft = hardwareMap.get(DcMotor.class, "Back Left");
+        FrontRight = hardwareMap.get(DcMotor.class, "Front Right");
 
-        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
-        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        FrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
         BackLeft.setDirection(DcMotor.Direction.REVERSE);
-        FrontRight.setDirection(DcMotor.Direction.FORWARD);
-        Shooter.setDirection(DcMotor.Direction.REVERSE);
-        Intake.setDirection(DcMotor.Direction.REVERSE);
-
-        //double minustrig =
-
+        FrontRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        //double minustrig = gamepad1.left_trigger *0.6;
-
         telemetry.addData("Left Stick X",gamepad1.left_stick_x);
         telemetry.addData("Right Stick X",gamepad1.right_stick_y);
         telemetry.addData("Right Stick Y",gamepad1.right_stick_y);
         telemetry.update();
-        y = -gamepad1.right_stick_y * 1;
-        x = -gamepad1.right_stick_x * 1.1 * -1;
-        rx = gamepad1.left_stick_x * 0.6 * 1;
+        y = gamepad1.right_stick_y * 1*0.5;
+        x = -gamepad1.right_stick_x * 1.1 * -0.5;
+        rx = gamepad1.left_stick_x * 0.6 * 0.5;
         denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(y), Math.abs(x), Math.abs(rx))), 1));
         BackLeft.setPower((y + x + rx) / denominator);
         FrontLeft.setPower(((y - x) + rx) / denominator);
         BackRight.setPower(((y - x) - rx) / denominator);
         FrontRight.setPower(((y + x) - rx) / denominator);
-        if (gamepad1.a){
-            Shooter.setPower(0.6 - (gamepad1.left_trigger*0.6));
-        } else if (gamepad1.b) {
-            Shooter.setPower(0);
-        }
-
-        Intake.setPower(gamepad1.right_trigger*0.75);
-
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMecanum.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
-
+@Disabled
 @TeleOp(name = "TestMecanum (Blocks to Java)")
 
 public class TestMecanum extends LinearOpMode {
