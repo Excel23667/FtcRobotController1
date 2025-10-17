fb2963a5931ac7034039f47cf54a982099e13b12
Excel23667 <ExcelRobotics23667@gmail.com>
2025-10-17 16:11:46 -0700

    iug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewChassis.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
@@ -19,6 +21,7 @@ public class NewChassis extends OpMode {
     private DcMotor Intake;
     private DcMotor Shooter;
     private DcMotor HighIntake;
+    private CRServo Piston;
     double y;
     double x;
     double rx;
@@ -34,16 +37,22 @@ public class NewChassis extends OpMode {
         Intake = hardwareMap.get(DcMotor.class, "Intake");
         HighIntake = hardwareMap.get(DcMotor.class, "High Intake");
         Shooter = hardwareMap.get(DcMotor.class, "Shooter");
+        Piston = hardwareMap.get(CRServo.class, "Piston");
 
         FrontLeft.setDirection(DcMotor.Direction.FORWARD);
         BackRight.setDirection(DcMotor.Direction.FORWARD);
         BackLeft.setDirection(DcMotor.Direction.REVERSE);
         FrontRight.setDirection(DcMotor.Direction.REVERSE);
+        Intake.setDirection(DcMotor.Direction.REVERSE);
+        HighIntake.setDirection(DcMotor.Direction.REVERSE);
 
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        Piston.setPower(0.01);
+        Piston.setPower(0);
     }
 
     @Override
@@ -52,19 +61,38 @@ public class NewChassis extends OpMode {
         telemetry.addData("Right Stick X",gamepad1.right_stick_y);
         telemetry.addData("Right Stick Y",gamepad1.right_stick_y);
         telemetry.update();
-        y = gamepad1.right_stick_y * 1*0.5;
-        x = -gamepad1.left_stick_x * 1.1 * -0.5;
-        rx = gamepad1.right_stick_x * 0.6 * -0.8;
+        y = gamepad1.right_stick_y * 1*0.75;
+        x = -gamepad1.left_stick_x * 1.1 * -0.75;
+        rx = gamepad1.right_stick_x * 0.6 * -0.9;
         denominator = JavaUtil.maxOfList(JavaUtil.createListWith(JavaUtil.sumOfList(JavaUtil.createListWith(Math.abs(y), Math.abs(x), Math.abs(rx))), 1));
         BackLeft.setPower((y + x + rx) / denominator);
         FrontLeft.setPower(((y - x) + rx) / denominator);
         BackRight.setPower(((y - x) - rx) / denominator);
         FrontRight.setPower(((y + x) - rx) / denominator);
         Intake.setPower(gamepad1.right_trigger * 0.6);
-        Shooter.setPower(gamepad1.left_trigger * 0.6);
+        HighIntake.setPower(gamepad1.left_trigger);
         if (gamepad1.a){
-            HighIntake.setPower(0.6);
+            Shooter.setPower( 0.75);
         }
+        if (gamepad1.x){
+            Shooter.setPower(0);
+        }
+        if (gamepad1.b){
+            Piston.setPower(-1);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
 
+            }
+            Piston.setPower(1);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+
+            }
+            Piston.setPower(0);
+        }
     }
 }
